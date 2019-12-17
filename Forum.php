0a48b3ccc560c2c8d2c6270c4bf5d2e81b8f5cb2
AdrienVanA <adririen.1@gmail.com>
2019-12-17 10:27:27 +0100

    commit 2

---
+++ Forum.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+encodage des pass dans la BDD, pas de claire
+-->
+
+  <?php   
+//$pseudo =$_SESSION['nickname']
+
+  ?>
+  <link href="Forum/style.css" rel="stylesheet" type="text/css" />
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Forum</title>
+   <?php     
+   include 'Login.php';
+    ?>
+    </head>
+    <body>
+      
+    </body>
+</html>
---
+++ inscription.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+  <?php
+  //
+   
+        ?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <p> 
+        <div>Pseudo 
+        <input id="login">
+        </div>
+        <div> Email
+        <input id="email">
+        </div>
+        </p>
+        
+        <p> 
+        <div>Nom 
+        <input id="nom">
+        </div>
+        <div> Prenom
+        <input id="prenom">
+        </div>
+        </p>      
+        
+        <p> 
+        <div>mot de passe 
+        <input id="password">
+        </div>
+        <div> confirmation mot de passe
+        <input id="password2">
+        </div>
+        </p>
+        <button id="BT_inscription">inscription</button>
+        
+        
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
